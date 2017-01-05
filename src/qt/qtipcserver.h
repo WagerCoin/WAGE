50e5c3bfb0b61825fbda25d3ee77b85b728bea99
Wager Development <WagerCoin@protonmail.com>
2017-01-05 12:38:11 -0800

    Initial upload
    Initial Wager client upload.

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Wager message queue name
+#define BITCOINURI_QUEUE_NAME "WagerURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
